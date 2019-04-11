b57c11543711c32d9dc4c7df69c6f5a610ff1899
santoshdkolur <48786464+santoshdkolur@users.noreply.github.com>
2019-04-11 22:05:00 +0530

    Create BFS.c

---
+++ BFS.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int cost[10][10],n,source,q[10],f,r,s[10],i,j,u,v;
+void bfs()
+{
+	for(i=1;i<=n;i++)
+		s[i]=0;
+	f=r=0;
+	q[r]=source;
+	s[source]=1;
+	while(f<=r)
+	{
+		u=q[f++];
+		for(v=1;v<=n;v++)
+		{
+			if(cost[v][v]&&s[v]==0)
+			{
+				s[v]=1;
+				q[++r]=v;
+
+			}
+		}
+	}
+}
+void main()
+{
+	printf("\nEnter no of vertices\n");
+	scanf("%d",&n);
+	printf("\nEnter the adjacency matrix\n");
+	for(i=1;i<=n;i++)
+		for(j=1;j<=n;j++)
+			scanf("%d",&cost[i][j]);
+		printf("\nEnter the source\n");
+		scanf("%d",&source);
+		bfs();
+		for(i=1;i<=n;i++)
+		{
+			if(s[i]==0)
+				printf("\nVertex %d not reachable\n",i);
+			else
+				printf("Vertex %d reachable\n",i);
+		}
+		for(i=1;i<=n;i++)
+			printf("\t%d",q[i]);
+	}
